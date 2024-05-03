6e673b492bfb7a611897150673a5066ab88565a4
spudg <coldhardbtc@proton.me>
2024-05-03 09:04:35 +0100

    First draft, still to add wojak image

--- src/App.js
+++ src/App.js
@@ -3,21 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page-container">
+      <h1 className="title">Bitcoin Wojak</h1>
+      <div className="container">
+        <div className="curved-box">
+          <ul className="link-list">
+            <li><a href="https://example1.com" target="_blank" rel="noopener noreferrer">X</a></li>
+            <li><a href="https://example2.com" target="_blank" rel="noopener noreferrer">Nostr</a></li>
+            <li><a href="https://example3.com" target="_blank" rel="noopener noreferrer">Medium</a></li>
+            <li><a href="https://example4.com" target="_blank" rel="noopener noreferrer">Fountain</a></li>
+            <li><a href="https://example5.com" target="_blank" rel="noopener noreferrer">YouTube</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }

10a0c8a46ba98c3d7b8330e2a7a0e40ab135660d
spudg <coldhardbtc@proton.me>
2024-05-04 13:46:16 +0100

    Added noreferrer

--- src/App.js
+++ src/App.js
@@ -36,16 +36,16 @@ function App() {
 <h2>wojak</h2>
       </div>
       <div className="wojak">
-        <img src="https://i.imgur.com/rXMBLwx.png" height="300px"/>
+        <img alt="wojak" src="https://i.imgur.com/rXMBLwx.png" height="300px"/>
       </div>
       <hr />
       <div className="container">
         <div className="link-list">
-            <div class="btn"><a target="_blank" href="https://twitter.com/btcwojak">X</a></div>
-            <div class="btn"><a target="_blank" href="https://primal.net/p/npub13r80695wvq4pk0upz4vyc9808ddpeal2jdtnmn7ufj3rwnx64pvqs433s6">Nostr</a></div>
-            <div class="btn"><a target="_blank" href="https://medium.com/@btcwojak">Medium</a></div>
-            <div class="btn"><a target="_blank" href="https://fountain.fm/btcwojak">Fountain</a></div>
-            <div class="btn"><a target="_blank" href="https://www.youtube.com/channel/UCAPA7vXbiW3o0HOoLRHzw-w">YouTube</a></div>
+            <div class="btn"><a target="_blank" rel="noreferrer" href="https://twitter.com/btcwojak">X</a></div>
+            <div class="btn"><a target="_blank" rel="noreferrer" href="https://primal.net/p/npub13r80695wvq4pk0upz4vyc9808ddpeal2jdtnmn7ufj3rwnx64pvqs433s6">Nostr</a></div>
+            <div class="btn"><a target="_blank" rel="noreferrer" href="https://medium.com/@btcwojak">Medium</a></div>
+            <div class="btn"><a target="_blank" rel="noreferrer" href="https://fountain.fm/btcwojak">Fountain</a></div>
+            <div class="btn"><a target="_blank" rel="noreferrer" href="https://www.youtube.com/channel/UCAPA7vXbiW3o0HOoLRHzw-w">YouTube</a></div>
         </div>
       </div>
       <hr />
